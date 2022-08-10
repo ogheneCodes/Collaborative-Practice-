c36042986f732467ad3a392680f61e867c27af5e
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-08-10 14:55:36 +0100

    Hour 2 and 3

--- firstFile.c
+++ firstFile.c
@@ -1,3 +1,26 @@
+// #include <stdio.h>
+// #include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-// first push
+
+// int greet(); //prototype
+
+// int main()
+// {
+//     printf("Howdy, neighbor! This is my first C program.\n");
+//     exit(0);
+    
+// }
+
+// int greet()
+// {
+//     printf("Howdy, neighbor! This is my first C program.\n");
+//     exit(0);
+// }
+void main()
+{
+    printf("Howdy, neighbor! This is my first C program.\n");
+    // return (1); // return is not used in void()
+    
+}
