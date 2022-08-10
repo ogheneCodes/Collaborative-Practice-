a6ee4bebb160814cbbfd10beb9fd8d1c02329d0e
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-08-10 15:58:02 +0100

    Hours 3

---
+++ hour3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// int integer_add(int x, int y)
+// {
+//     int result;
+//     result = x + y;
+//     return result;
+// }
+// int main()
+// {
+//     int A;
+//     int B;
+//     int C;
+//     A = integer_add(5, 12);
+//     B = integer_add(5, 10);
+//     C = integer_add(5, 11);
+//     printf("The addition of 5 and 12 is % d.\n", A);
+//     printf("The addition of 5 and 12 is % d.\n", B);
+//     printf("The addition of 5 and 12 is % d.\n", C);
+//     return 0;
+// }
+
+// Constant and Variables
+
+// int main(){
+//     const int x = 2;
+//     printf("The value is: %d\n", x);
+//     x = ++x;
+//     printf("The value is: %d\n", x);
+
+
+//     return 0;
+// }
+
+// int integer_add( int x, int y, int z)
+// {
+// int sum;
+// sum = x + y + z;
+// return sum;
+// }
+
+// int main()
+// {
+//     int x; //declaration
+//     x = 3; //intialization
+//     int y = 5 + x; //definition
+//     printf("Y = %d", y);
+//     return 0;
+
+// }
+
+/*Write a C function that can perform a multiplication
+ of two integers and return the
+calculated result.*/
+
+int multiply(int x, int y){
+    int result = x * y;
+    printf("%d * %d = %d", x,y, result);
+    return 0;
+}
+
+int main(){
+    multiply(12, 2);
+}
