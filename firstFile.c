8094c70c2b57dae174ac6f457e65f9c04c156492
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-08-10 15:14:10 +0100

    Addition task

--- firstFile.c
+++ firstFile.c
@@ -2,25 +2,41 @@
 // #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
 
 
 // int greet(); //prototype
+int add(a,b)
+{
+    int z = a + b;
+     printf("This is the addition of A and B: %d\n", z);
+    return 0;
+}
 
-// int main()
-// {
-//     printf("Howdy, neighbor! This is my first C program.\n");
-//     exit(0);
+int main()
+{
+    add(4, 4);
+    // int a = 2;
+    // int b = 4;
+    // int z = add(a,b);
+    // printf("%d\n", z);
+    return 0;
+
+
+    // printf("Howdy, neighbor! This is my first C program.\n");
+    // return (0);
     
-// }
+}
 
 // int greet()
 // {
 //     printf("Howdy, neighbor! This is my first C program.\n");
 //     exit(0);
 // }
-void main()
-{
-    printf("Howdy, neighbor! This is my first C program.\n");
-    // return (1); // return is not used in void()
+// void main()
+// {
+//     printf("Howdy, neighbor! This is my first C program.\n");
+//     // return (1); // return is not used in void()
     
-}
+// }
