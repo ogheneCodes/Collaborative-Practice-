d9ffd36f5003798fe7b0a60a82bd3b849fa61453
Olawalesegun <olawalesegun0@gmail.com>
2022-08-13 12:30:28 +0000

    Exercise 1 of Hour 4 collaboarative Practice

---
+++ Segun/Hour4_DatatypesKeywords/task1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numeric values.
+ * @file1: contains the first value.
+ * @file2: contains the second value.
+ *
+ * retuns 0.
+ */
+int main()
+{
+	char file1 = 'Z';
+	char file2 = 'z';
+	printf("The numeric value of Z is: %d", file1);
+	printf("The numeric value of z is: %d", file2);
+	return 0;
+}
