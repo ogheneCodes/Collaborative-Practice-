2cdb59a2f17cc39a9b78dc59ffdcad559e2ac7af
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-08-14 21:02:40 +0100

    newline

---
+++ Michael's Practice/newline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - code
+ * Description: prints the numeric value of \n
+ * 
+ * Return: 0
+ * 
+ */
+int main(void)
+{
+    int a = '\n';
+
+    printf("The numeric value of \\n is: %d", a);
+
+    return (0);
+}
