3382ff1459aa51efe48d8a18a107448a75da52b6
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-08-14 11:11:39 +0100

    Hour 7

---
+++ MAX/hour7/exercise2.c
@@ -0,0 +1,17 @@
+/*
+Write a program that contains the two pieces of code shown 
+in Exercise 1, and then execute the program. What are you going to 
+see on the screen?
+for (i = 0, j = 1; i < 8; i++, j++)
+//         printf("%d + %d = %d\n", i, j, i + j);
+
+for (i=0, j=1; i<8; i++, j++); //semiclon
+//         printf("%d + %d = %d\n", i, j, i+j);
+*/
+
+int main()
+{
+    
+}
+
+// Errors of compilation
