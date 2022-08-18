0de64a541d2b1dee0c45efa51cf18a8a1924d362
MuneneCalvin <munenecalvn.com>
2022-08-18 09:41:10 +0300

    Chapter 4 & 5

---
+++ Calvin/Chapter 4/task 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the numeric values of Z and z.
+*
+*/
+
+int main(void)
+{
+    printf("The numeric value of Z is: %d\n", 'Z');
+    printf("The numeric value of z is: %d\n", 'z');
+    return 0;
+}
---
+++ Calvin/Chapter 5/Task 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - puts the given char together using putchar.
+* return 0 when succesfull
+*
+*/
+
+int main(void)
+{
+    putchar('B');
+    putchar('y');
+    putchar('e');
+
+    return 0;
+}
