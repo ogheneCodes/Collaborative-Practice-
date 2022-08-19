53cd1671fcfda9fc02edf99e9764d07f55509c3b
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-19 12:33:53 +0300

    Added some file on Arrays and pointers

---
+++ Joseph Njoroge/Chapter 13/task4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+4. Write a program that uses the scanf() function to read in two integers entered by
+the user, adds the two integers, and then prints out the sum on the screen.
+
+*/
+
+int main(void)
+{
+    int x , y;
+    printf("Enter the 2 values: \n");
+    scanf("%d %d, &x, &y");
+
+    printf("The sum of the ints are: %d", x + y);
+    return 0;
+}
