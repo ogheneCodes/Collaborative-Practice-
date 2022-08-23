41c2bc1ccc006539818a7967dedcfbf0c1ab8197
magicbett <stanley.kariba@yahoo.com>
2022-08-23 13:55:05 +0300

    Chapter 3

---
+++ Moses_Bett/04L01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+	char c1;
+	char c2;
+
+	c1 = 'A';
+	c2 = 'a';
+	printf("Convert the value of c1 to character: %c.\n", c1);
+	printf("Convert the value of c2 to character: %c.\n", c2);
+	return (0);
+}
