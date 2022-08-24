b6bffd1cb55df20f9a61b5b0268efc1035cfaf2d
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-08-24 12:17:03 +0300

    Hour 9

--- Mutai-Gilbert/Hour9/6.c
+++ Mutai-Gilbert/Hour9/6.c
@@ -1,1 +1,9 @@
 //6. Write a program to calculate the non-negative square root of 0x19A1.
+#include <stdio.h>
+#include <math.h>
+int main(){
+    unsigned int x = 0x19A1;
+    printf("decimal value of x is: %d\n", x);
+    printf("sqrt(x) returns: %2.0f\n", sqrt(x));
+    return 0;
+    }
