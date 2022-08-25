d76b1b99d137f06782d75ce3f87d9cb05e051d06
Olawalesegun <olawalesegun0@gmail.com>
2022-08-25 09:42:04 -0700

    Task Hour 8

---
+++ Segun/Hour8_Con-Operators/Task3.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+int main(void)
+{
+	int x, y;
+
+	x = 123;
+	y = 4;
+	printf(" x is right shifted to become: %d\n", x >> y);
+	printf(" y is left shifted to become: %d\n", x << y);
+}
