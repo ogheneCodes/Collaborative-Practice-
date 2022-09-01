076236beced467aeb7fc3730df68a4866df62413
Olawalesegun <Olawalesegun0@gmail.com>
2022-09-01 17:36:09 +0000

    Task 1 Hour12

---
+++ Segun/Hour12_Arrays/Task1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char array_ch[5] = {'A', 'B', 'C', 'D', 'E'};
+
+	printf("The value of array_ch[0] is: %c\n", array_ch[0]);
+	printf("The value of array_ch[1] is: %c\n", array_ch[1]);
+	printf("The value of array_ch[2] is: %c\n", array_ch[2]);
+	printf("The value of array_ch[3] is: %c\n", array_ch[3]);
+	printf("The value of the last array value: %c\n", array_ch[4]);
+	return 0;
+}
