4ac5c0d597b4d0529c6f276db5a090f362af0e7c
horace17 <73630780+horace17@users.noreply.github.com>
2022-09-01 03:15:56 +0300

    Task 4

---
+++ Horace/Chapter_12/Exercise2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    char array_ch[5];
+    for(i = 0; i < 5; i++){
+        array_ch[i] = 'a' + i;
+    }
+    for(i = 0; i < 5; i++){
+        printf("%c ", array_ch[i]);
+    }
+}
