b44f3ec10a9c4503f81ecae2341e358e8fa20be9
magicbett <stanley.kariba@yahoo.com>
2022-09-01 14:55:45 +0300

    hour 14 is the most challenging hour so far

---
+++ Practice/hour14/exercise3.c
@@ -0,0 +1,16 @@
+ //Compile and run the following program. What do you get on the screen, and why?
+
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i=0; i<5; i++)
+	{
+		int x = 1;
+		static int y = 0;
+		printf("x=%d, y=%d\n", x++, y++);
+	}
+	return 0;
+}
---
+++ Practice/hour14/exercise4.c
@@ -0,0 +1,25 @@
+/*
+Rewrite the add_two() function in Listing 14.3 to print out the previous result of
+the addition, as well as the counter value
+*/
+
+#include <stdio.h>
+/* the add_two function */
+int add_two(int x, int y)
+{
+	static int counter = 1;
+ 	printf("This is counter value: %d\n", counter); 
+	printf("This is the function call of %d,\n", counter++);
+//	printf("This is counter value: %d\n", counter);
+	return (x + y);
+}
+/* the main function */
+
+int  main()
+{
+ 	int i, j;
+	
+	for (i=0, j=5; i<5; i++, j--)
+	printf("the addition of %d and %d is %d.\n\n",i, j, add_two(i, j));
+return 0;
+}
