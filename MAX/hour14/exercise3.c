9ce89b385b3129394b1319f8af29ea95588c8199
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-09-04 14:02:18 +0100

    hour 14

---
+++ MAX/hour14/exercise3.c
@@ -0,0 +1,15 @@
+/*
+Compile and run the following program. What do you get on the screen, and why?
+#include <stdio.h>
+*/
+int main()
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        int x = 0;
+        static int y = 0;
+        printf("x=%d, y=%d\n", x++, y++);
+    }
+    return 0;
+}
