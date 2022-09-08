cd6e7bf1ae2bc823f865d1059331b9348bf9ad50
MuneneCalvin <munenecalvn.com>
2022-09-08 16:21:45 +0300

    Hour 15

--- Practice/hour15/exercise3.c
+++ Practice/hour15/exercise3.c
@@ -1,38 +1,34 @@
-/* Rewrite the program in Listing 15.3. This time, make a function that takes a vari-
-able number of int arguments and performs the operation of multiplication on
-these arguments.*/
+/*3. Rewrite the program in Listing 15.3. 
+This time, make a function that takes a vari- able number of int arguments and 
+performs the operation of multiplication on these arguments.
+*/
 /* 15L03.c: Processing variable arguments */
 #include <stdio.h>
 #include <stdarg.h>
-
-int Mult(int x, ...);
-
-int main () {
-    int d1 = 5;
-    int d2 = 2;
-    int d3 = 3;
-    int d4 = 4;
-    printf("Given an argument: %d\n", d1);
-    printf("The result returned by Mult is: %d\n\n", Mult(1, d1));
-    printf("Given arguments: %d and %d\n", d1, d2);
-    printf("The result returned by Mult is: %d\n\n", Mult(2, d1, d2));
-    printf("Given arguments: %d, %d and %d\n", d1, d2, d3);
-    printf("The result returned by Mult is: %d\n\n", Mult(3, d1, d2, d3));
-    printf("Given arguments: %d, %d, %d, and %d\n", d1, d2, d3, d4);
-    printf("The result returned by Mult is: %d\n", Mult(4, d1, d2, d3, d4));
-return 0;
+double multDouble(int x, ...);
+int main (){
+    double d1 = 1.5; 
+    double d2 = 2.5; 
+    double d3 = 3.5; 
+    double d4 = 4.5;
+    printf("Given an argument: %2.1f\n", d1);
+    printf("The result returned by multDouble() is: %2.3f\n\n",multDouble(1, d1));
+    printf("Given arguments: %2.1f and %2.1f\n", d1, d2); 
+    printf("The result returned by multDouble() is: %2.3f\n\n",multDouble(2, d1, d2));
+    printf("Given arguments: %2.1f, %2.1f and %2.1f\n", d1, d2, d3); 
+    printf("The result returned by multDouble() is: %2.3f\n\n",multDouble(3, d1, d2, d3));
+    printf("Given arguments: %2.1f, %2.1f, %2.1f, and %2.1f\n",d1, d2, d3, d4);
+    printf("The result returned by multDouble() is: %2.3f\n",multDouble(4, d1, d2, d3, d4)); 
+    return 0;
 }
-
-/* definition of AddDouble() */
-int Mult(int x, ...)
-{
-    va_list  arglist;
+double multDouble(int x, ...){
+    va_list arglist;
     int i;
-    int result = 1;
+    double result = 1;
     printf("The number of arguments is: %d\n", x);
     va_start (arglist, x);
-        for (i=0; i<x; i++)
-        result *= va_arg(arglist, int);
+    for (i=0; i<x; i++)
+    result *= va_arg(arglist, double);
     va_end (arglist);
-return result;
+    return result;
 }
