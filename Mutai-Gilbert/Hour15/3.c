bf19481af79db877c683fbe82612447f508edbf7
Gilbert Mutai <mutai@MacBook-Pro.local>
2022-09-11 21:59:22 +0300

    Hour16

--- Mutai-Gilbert/Hour15/3.c
+++ Mutai-Gilbert/Hour15/3.c
@@ -12,13 +12,13 @@ int main (){
     double d3 = 3.5; 
     double d4 = 4.5;
     printf("Given an argument: %2.1f\n", d1);
-    printf("The result returned by multDouble() is: %2.1f\n\n",multDouble(1, d1));
+    printf("The result returned by multDouble() is: %3.1f\n\n",multDouble(1, d1));
     printf("Given arguments: %2.1f and %2.1f\n", d1, d2); 
-    printf("The result returned by multDouble() is: %2.1f\n\n",multDouble(2, d1, d2));
+    printf("The result returned by multDouble() is: %3.1f\n\n",multDouble(2, d1, d2));
     printf("Given arguments: %2.1f, %2.1f and %2.1f\n", d1, d2, d3); 
-    printf("The result returned by multDouble() is: %2.1f\n\n",multDouble(3, d1, d2, d3));
+    printf("The result returned by multDouble() is: %3.1f\n\n",multDouble(3, d1, d2, d3));
     printf("Given arguments: %2.1f, %2.1f, %2.1f, and %2.1f\n",d1, d2, d3, d4);
-    printf("The result returned by multDouble() is: %2.1f\n",multDouble(4, d1, d2, d3, d4)); 
+    printf("The result returned by multDouble() is: %3.1f\n",multDouble(4, d1, d2, d3, d4)); 
     return 0;
  }
 double multDouble(int x, ...){
---
+++ Mutai-Gilbert/Hour16/3.c
@@ -0,0 +1,5 @@
+/*
+3. Given a two-dimensional character array,
+ str, that is initialized as char str[2][15] = { “You know what,”, “C is powerful.” };
+write a program to pass the start address of str to a function that prints out the content of the character array.
+*/
