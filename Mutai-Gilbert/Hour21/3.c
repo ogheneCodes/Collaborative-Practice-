e9bfb155bddc592995110a8255cc0eb9b7ce7afd
mutai-gilbert <mutaigilbert85@gmail.com>
2022-09-29 14:36:22 +0300

    Hour 21

---
+++ Mutai-Gilbert/Hour21/3.c
@@ -0,0 +1,5 @@
+/*
+3. Given the string “Disk file I/O is fun.” 
+write a program to write the string into a file called test_21.txt by writing one character at a time. 
+Meanwhile, print out the string on the screen.
+*/
---
+++ Mutai-Gilbert/Hour21/4.c
@@ -0,0 +1,3 @@
+/*
+
+4. Rewrite exercise 3. This time, try to write one block of characters (that is, one string) at a time.*/
